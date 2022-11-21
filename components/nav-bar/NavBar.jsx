8045d973f956a174feba671041718ea9d6f0c52a
Thabiso-Timzo <mr.tt.hlatshwayo@gmail.com>
2022-11-21 22:58:33 +0200

    Initial commit

---
+++ components/nav-bar/NavBar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const NavBar = () => {
+  return (
+    <div className='fixed w-full h-20 shadow-xl z-[100]'> 
+      <div className=''>
+
+      </div>
+    </div>
+  )
+}
+// 18:14
+export default NavBar
