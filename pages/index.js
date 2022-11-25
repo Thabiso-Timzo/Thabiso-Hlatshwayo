0ac27fddd765e76357a107c2b86589aac935c13a
Thabiso-Timzo <mr.tt.hlatshwayo@gmail.com>
2022-11-25 11:46:01 +0200

    Modified index.js

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import Head from 'next/head'
 import NavBar from '../components/nav-bar/NavBar'
 import About from './about/About'
 import Main from './main/Main'
+import Skills from './skills/Skills'
 
 export default function Home() {
   return (
@@ -15,6 +16,7 @@ export default function Home() {
       <NavBar />
       <Main />
       <About />
+      <Skills />
     </div>
   )
 }
