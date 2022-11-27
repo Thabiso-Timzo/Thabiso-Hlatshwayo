9898ea75b91413aba4a3ef0a263404f29633c83b
Thabiso-Timzo <mr.tt.hlatshwayo@gmail.com>
2022-11-27 21:21:52 +0200

    Modified navbar

--- components/nav-bar/NavBar.jsx
+++ components/nav-bar/NavBar.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
 import { 
@@ -11,40 +11,54 @@ import {
 } from 'react-icons/ai'
 
 import logo from '../../public/assets/logo/logo.png'
-
+// 1:58:37
 const NavBar = () => {
   const [nav, setNav] = useState(false)
+  const [shadow, setShadow] = useState(false)
 
   const handleNav = () => {
     setNav(!nav)
   }
 
+  useEffect(() => {
+    const handleShadow = () => {
+      if (window.scrollY >= 90) {
+        setShadow(true)
+      } else {
+        setShadow(false)
+      }
+    } 
+    window.addEventListener('scroll', handleShadow)
+  }, [])
+
   return (
     <div 
-      className={'fixed w-full h-[60px] shadow-xl z-[100]'}> 
+      className={shadow ? 'fixed w-full h-[60px] shadow-xl z-[100]' : 'fixed w-full h-[60px] z-[100]'}> 
       <div 
         className={'flex justify-between items-center w-full h-full px-2 2xl:px-16'}>
-        <Image 
-          src={logo} 
-          alt='' 
-          width={'125'} 
-          height={'50'}
-        />
+        <Link href={'/'}>
+          <Image 
+            src={logo} 
+            alt='' 
+            width={'125'} 
+            height={'50'}
+          />
+        </Link>
         <div>
           <ul className={'hidden md:flex'}>
             <Link href={'/'}>
               <li className={'ml-10 text-sm uppercase hover:border-b'}>Home</li>
             </Link>
-            <Link href={'/'}>
+            <Link href={'/about'}>
               <li className={'ml-10 text-sm uppercase hover:border-b'}>About</li>
             </Link>
-            <Link href={'/'}>
+            <Link href={'/skills'}>
               <li className={'ml-10 text-sm uppercase hover:border-b'}>Skills</li>
             </Link>
-            <Link href={'/'}>
+            <Link href={'/projects'}>
               <li className={'ml-10 text-sm uppercase hover:border-b'}>Projects</li>
             </Link>
-            <Link href={'/'}>
+            <Link href={'/contact'}>
               <li className={'ml-10 text-sm uppercase hover:border-b'}>Contact</li>
             </Link>
           </ul>
@@ -62,12 +76,14 @@ const NavBar = () => {
         >
           <div className="">
             <div className={'flex w-full items-center justify-between'}>
-              <Image 
-                src={logo} 
-                alt='' 
-                width={'100'} 
-                height={'50'}
-              />
+              <Link href={'/'}>
+                <Image 
+                  src={logo} 
+                  alt='' 
+                  width={'100'} 
+                  height={'50'}
+                />
+              </Link>
               <div onClick={handleNav} className={'rounded-full shadow-lg shadow-gray-700 p-3 cursor-pointer'}>
                 <AiOutlineClose size={25} />
               </div>
