2746cfcf980ca0b1a04814c3ce8accb1c3fb2425
Thabiso-Timzo <mr.tt.hlatshwayo@gmail.com>
2022-12-01 17:22:06 +0200

    Removed data file

--- data/about-data/about.js
+++
@@ -1,6 +0,0 @@
-export const about = [
-    {
-        id: 0,
-        text: "I am a highly competent IT professional with a proven track record in designing websites, networking and managing databases. I have strong technical skills as well as excellent interpersonal skills, enabling me to interact with clients. I am eager to be challenged in order to grow and further improve my IT skills. My greatest passion is in life is using my technical know-how to benefit other people and organisations"
-    }
-]
