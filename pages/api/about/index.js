293c6b5daedca6f89bff04b753d8308605139216
Thabiso-Timzo <mr.tt.hlatshwayo@gmail.com>
2022-12-02 10:18:12 +0200

    Remcved server side about file

--- pages/api/about/index.js
+++
@@ -1,9 +0,0 @@
-const { about } = require('../../../data/about-data/about')
-
-export default function aboutData(req, res) {
-    try {
-        return res.status(200).json(about)   
-    } catch (error) {
-        return res.status(500).json({message: error.message})
-    }
-}
