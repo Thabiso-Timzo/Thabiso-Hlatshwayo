dfbbce2bc27f864ca8c00a65ae3dfd74103c8310
Thabiso-Timzo <mr.tt.hlatshwayo@gmail.com>
2022-12-02 14:35:54 +0200

    Added form validation

---
+++ utils/validation/validation.js
@@ -0,0 +1,11 @@
+// Checks if form fields are empty
+export const isEmpty = (value) => {
+    if (!value) return true
+    return false
+}
+
+// checks if email address is valid
+export const isEmail = (email) => {
+    const regex = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return regex.test(email)
+}
