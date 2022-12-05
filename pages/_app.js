b08bec0960a7ed784c6bf03087fa8f8e6e1c33bb
Thabiso-Timzo <mr.tt.hlatshwayo@gmail.com>
2022-12-05 20:45:15 +0200

    Updated folder

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,14 @@
-import {Provider} from "react-redux";
-import { ToastContainer, toast } from 'react-toastify';
+import { ToastContainer } from 'react-toastify';
 import "react-toastify/dist/ReactToastify.css"
 
 import '../styles/globals.css'
-import store from '../store/store'
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Provider store={store}>
+    <>
       <Component {...pageProps} />
       <ToastContainer />
-    </Provider>
+    </>
   )
 }
 
